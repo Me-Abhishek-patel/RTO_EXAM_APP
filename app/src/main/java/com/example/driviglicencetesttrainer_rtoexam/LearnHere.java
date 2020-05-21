88dad2a6fedb9c59ad4d515856f28f8a77e013e3
Abhishek Patel <patelabhishek2007@gmail.com>
2020-05-21 18:17:16 +0530

    Added all Questions to Learn Here

--- app/src/main/java/com/example/driviglicencetesttrainer_rtoexam/LearnHere.java
+++ app/src/main/java/com/example/driviglicencetesttrainer_rtoexam/LearnHere.java
@@ -39,4 +39,83 @@ public class LearnHere extends AppCompatActivity {
 
     }
 
+    public void qPracticeSet4(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet4Intent = new Intent(this, learn.class);
+        qpSet4Intent.putExtra("set", 4);
+        startActivity(qpSet4Intent);
+
+    }
+
+    public void qPracticeSet5(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet5Intent = new Intent(this, learn.class);
+        qpSet5Intent.putExtra("set", 5);
+        startActivity(qpSet5Intent);
+
+    }
+
+    public void qPracticeSet6(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet6Intent = new Intent(this, learn.class);
+        qpSet6Intent.putExtra("set", 6);
+        startActivity(qpSet6Intent);
+
+    }
+
+    public void qPracticeSet7(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet7Intent = new Intent(this, learn.class);
+        qpSet7Intent.putExtra("set", 7);
+        startActivity(qpSet7Intent);
+
+    }
+
+    public void qPracticeSet8(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet8Intent = new Intent(this, learn.class);
+        qpSet8Intent.putExtra("set", 8);
+        startActivity(qpSet8Intent);
+
+    }
+
+    public void qPracticeSet9(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet9Intent = new Intent(this, learn.class);
+        qpSet9Intent.putExtra("set", 9);
+        startActivity(qpSet9Intent);
+
+    }
+
+    public void qPracticeSet10(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet10Intent = new Intent(this, learn.class);
+        qpSet10Intent.putExtra("set", 10);
+        startActivity(qpSet10Intent);
+
+    }
+
+    public void qPracticeSet11(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet11Intent = new Intent(this, learn.class);
+        qpSet11Intent.putExtra("set", 11);
+        startActivity(qpSet11Intent);
+
+    }
+
+    public void qPracticeSet12(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet12Intent = new Intent(this, learn.class);
+        qpSet12Intent.putExtra("set", 12);
+        startActivity(qpSet12Intent);
+
+    }
+
+    public void qPracticeSet13(View view) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+        Intent qpSet13Intent = new Intent(this, learn.class);
+        qpSet13Intent.putExtra("set", 13);
+        startActivity(qpSet13Intent);
+
+    }
 }
